d9f40a3cdf47bb3031c72ea0ec5d3d7f71d2ba47
MATTHEW SIMMS <M.PARKER.SIMMS@GMAIL.COM>
2021-05-28 00:40:38 -0500

    got it all working for the most part

--- app/src/main/java/com/andyagulue/github/jammin/activities/LogInPage.java
+++ app/src/main/java/com/andyagulue/github/jammin/activities/LogInPage.java
@@ -1,18 +1,26 @@
 package com.andyagulue.github.jammin.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.amplifyframework.api.graphql.model.ModelQuery;
 import com.amplifyframework.core.Amplify;
+import com.amplifyframework.datastore.generated.model.Musician;
 import com.andyagulue.github.jammin.R;
 
 public class LogInPage extends AppCompatActivity {
     String TAG = "loginPage";
     TextView username1;
+    String username2;
+    Musician musician;
+    Handler loginPageHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,16 +33,15 @@ public class LogInPage extends AppCompatActivity {
 
         findViewById(R.id.loginButton).setOnClickListener(v -> {
             String password = ((TextView) findViewById(R.id.loginPasswordEditText)).getText().toString();
-            String username2= username1.getText().toString();
+            username2 = username1.getText().toString();
 
             Amplify.Auth.signIn(
                     username2,
                     password,
                     response -> {
                         Log.i(TAG, "Successful login of user" + response);
-                        Intent intent = new Intent(LogInPage.this, CreateProfilePage.class);
-                        intent.putExtra("username", username);
-                        startActivity(intent);
+                        loginPageHandler.sendEmptyMessage(127);
+
                     },
                     error -> {
                         Log.i(TAG, "unsuccessful login of user" + error.toString());
@@ -42,5 +49,36 @@ public class LogInPage extends AppCompatActivity {
             );
 
         });
+        loginPageHandler = new Handler(this.getMainLooper()) {
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                super.handleMessage(msg);
+                if (msg.what == 127) {
+                    Amplify.API.query(
+                            ModelQuery.list(Musician.class, Musician.USERNAME.eq(username2)),
+                            resp -> {
+                                Log.i(TAG, "handleMessage:" + resp);
+                                if(musician != null) {
+                                    Intent intent = new Intent(LogInPage.this, DiscoverPage.class);
+                                    intent.putExtra("username", username2);
+                                    startActivity(intent);
+                                }else{
+                                    Intent intent = new Intent(LogInPage.this, CreateProfilePage.class);
+                                    intent.putExtra("username", username2);
+                                    startActivity(intent);
+                                }
+
+
+                            },
+                            error -> {}
+                    );
+                }
+            }
+        };
     }
 }
+
+
+
+
+
